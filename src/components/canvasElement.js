3e7eed37bbe2c63b362a3105d0a38e026bf9104f
akramhar <alex.83@bluewin.ch>
2019-02-19 15:37:47 -0500

    Work in progress

--- src/components/canvasElement.js
+++ src/components/canvasElement.js
@@ -1,24 +1,37 @@
 import React from "react";
+import { Resizable, ResizableBox } from "react-resizable";
+
+import "react-resizable/css/styles.css";
 
 export class CanvasElement extends React.PureComponent {
 	render() {
 		// eslint-disable-next-line no-console
 		console.log("testCanvasElement");
 		const style = {
-			backgroundColor: "white",
+			backgroundColor: "grey",
 			textAlign: "center",
-			height: "50",
-			width: "50"
+			height: "100%",
+			width: "100%"
 		};
 		return (
-			<button style={style} onClick={this.props.onClick}>
-				{this.props.text}
-			</button>
+			<ResizableBox
+				width={100}
+				height={100}
+				minConstraints={[100, 100]}
+				maxConstraints={[this.props.maxWidth, this.props.maxHeight]}
+				lockAspectRatio={true}
+			>
+				<button style={style} onClick={this.props.onClick}>
+					{this.props.text}
+				</button>
+			</ResizableBox>
 		);
 	}
 }
 
 CanvasElement.defaultProps = {
+	maxWidth: 200,
+	maxHeight: 200,
 	text: "Something",
 	onClick: function(e) {
 		// eslint-disable-next-line no-console
