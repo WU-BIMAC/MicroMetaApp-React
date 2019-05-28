6986bc3eb855fd7da5aa5232bd55b4109eab3431
akramhar <alex.83@bluewin.ch>
2019-05-28 12:53:15 -0400

    work in progress

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
 		filename:
 			mode === "production"
 				? "MicroscopyMetadataTool.min.js"
-				: "MicroscopyMetadataTool.dev.js"
+				: "MicroscopyMetadataTool.js"
 	},
 	module: {
 		rules: [
