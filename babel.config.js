80e6426bf98ac5118bbee7bea0c3287dfbee7bdb
utku-ozturk <utku.ozturk@karyasmd.com>
2021-07-07 13:30:28 +0300

    babel.config.js update

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,10 @@
-module.exports = function(api) {
+module.exports = function (api) {
 	api.cache(true);
 	return {
-		presets: ["@babel/preset-env", "@babel/preset-react"],
+		presets: [
+			["@babel/preset-env", { "modules": false }],
+			"@babel/preset-react"
+		],
 		plugins: [
 			"@babel/plugin-syntax-dynamic-import",
 			"@babel/plugin-proposal-object-rest-spread",
