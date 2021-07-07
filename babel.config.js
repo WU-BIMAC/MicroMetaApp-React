c3b6db7962fbe61af4e77ceb39c241bf46ca4e5b
utku-ozturk <utku.ozturk@karyasmd.com>
2021-07-07 17:39:19 +0300

    Revert "babel.config.js update"
    This reverts commit 80e6426bf98ac5118bbee7bea0c3287dfbee7bdb.

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,7 @@
-module.exports = function (api) {
+module.exports = function(api) {
 	api.cache(true);
 	return {
-		presets: [
-			["@babel/preset-env", { "modules": false }],
-			"@babel/preset-react"
-		],
+		presets: ["@babel/preset-env", "@babel/preset-react"],
 		plugins: [
 			"@babel/plugin-syntax-dynamic-import",
 			"@babel/plugin-proposal-object-rest-spread",
