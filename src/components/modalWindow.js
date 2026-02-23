0b51b77cc64955e6469a8176c23487980ddebfbf
akramhar <alex.83@rigano.ch>
2026-02-22 19:52:09 -0500

    added zIndex

--- src/components/modalWindow.js
+++ src/components/modalWindow.js
@@ -11,6 +11,7 @@ export default class ModalWindow extends React.PureComponent {
 					top: 0,
 					bottom: 0,
 					right: 0,
+					zIndex: 1000,
 					backgroundColor: "rgba(0,0,0,0.33)",
 					display: "flex",
 					alignItems: "center",
